bcec4f355e097b77a58396aac06eac6554578b5d
Ashoka Shringla <ashokashringla@gmail.com>
2022-02-03 14:24:25 -0500

    added files

---
+++ README.md
@@ -0,0 +1,21 @@
+## Javalin Forms
+---
+### Description
+
+In this lab you will add more parameters to a pre-existing form and modify the resulting print out.
+
+---
+### Steps
+
+ 
+1. In src -> main -> jte -> form.jte modify the file to add a favorite color segment. 
+2. In src -> main -> java -> Main.java in the handler create a new string to hold the form parameter value for the favorite color.
+3. Main.java modify the ctx result to include a part for the new color segment.
+
+
+---
+### Sample Output
+```Java
+"Hello your name is Andy and your favorite color is pink!"
+```
+---
---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import io.javalin.Javalin;
+import java.util.*;
+import io.javalin.http.Handler;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(4100);
+
+        app.get("/", ctx -> ctx.render("index.jte"));
+
+        app.get("/getReimbursement", getFormHandler);
+
+        app.post("/wrongRoute", postFormHandler);
+
+    }
+
+    public static Handler getFormHandler = ctx -> {
+
+        ctx.render("form.jte");
+
+    };
+
+    public static Handler postFormHandler = ctx -> {
+
+        String email = ctx.formParam("email");
+        String item = ctx.formParam("item");
+        String amount = ctx.formParam("amount");
+
+        ctx.result("Your email is " + email + ". You are applying for reimbursement for " + item + " and the amount is $" + amount);
+
+    };
+
+}
